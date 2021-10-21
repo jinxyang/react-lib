f2bb4d43d2ece29325d4fdc09da9f44bb57f4fca
jinxyang <jinx@jinxyang.com>
2021-10-21 17:17:53 +0800

    feat: add IconSelect to Form components

--- src/components/Form/components.js
+++ src/components/Form/components.js
@@ -1,21 +1,23 @@
 import { InputNumber } from 'antd'
 import DatePicker from '../DatePicker'
+import IconSelect from '../IconSelect'
 import Input from '../Input'
-import Password from '../Password'
 import Radio from '../Radio'
 import RangePicker from '../RangePicker'
+import Password from '../Password'
 import Select from '../Select'
 import Switch from '../Switch'
 import TreeSelect from '../TreeSelect'
 
 export default {
+  DatePicker,
+  IconSelect,
   Input,
   Number: InputNumber,
-  Password,
   Radio,
+  RangePicker,
+  Password,
   Select,
   Switch,
-  DatePicker,
-  RangePicker,
   TreeSelect,
 }
