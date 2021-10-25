08017b38e10592863e43eba56795eaa348bd0f56
jinxyang <jinx@jinxyang.com>
2021-10-25 17:28:36 +0800

    fix: labelInValue in Tree

--- src/components/Tree/Tree.jsx
+++ src/components/Tree/Tree.jsx
@@ -3,7 +3,7 @@ import { Tree } from 'antd'
 
 const getTreeNode = (list = []) =>
   list.map(({ label, value, props = {}, children }) => (
-    <Tree.TreeNode {...props} key={value} value={value} title={label}>
+    <Tree.TreeNode {...props} key={value} title={label}>
       {children && getTreeNode(children)}
     </Tree.TreeNode>
   ))
@@ -12,16 +12,20 @@ const ProTree = ({
   value = '',
   options = [],
   onChange = () => {},
+  checkable = false,
+  labelInValue = false,
   ...props
 }) => {
+  const restProps = checkable
+    ? {
+        checkedKeys: value,
+        onCheck: ({ checked }, { checkedNodes }) => {
+          onChange(labelInValue ? checkedNodes : checked)
+        },
+      }
+    : { selectedKeys: value, onSelect: onChange }
   return (
-    <Tree
-      {...props}
-      checkedKeys={value}
-      selectedKeys={value}
-      onSelect={onChange}
-      onCheck={onChange}
-    >
+    <Tree {...props} {...restProps} checkable={checkable}>
       {getTreeNode(options)}
     </Tree>
   )
