94a313d656de30e76e8008ff8c849960ef21b50e
jinxyang <jinx@jinxyang.com>
2021-10-27 16:01:24 +0800

    feat: update NotFound

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
+import App from '../App'
+import StyledCenter from '../styled/StyledCenter'
+
 const NotFound = () => {
-  return <div>Not Found</div>
+  return (
+    <App fill={false} color="red" opacity={0.5}>
+      <StyledCenter>权限不足，请联系管理员</StyledCenter>
+    </App>
+  )
 }
 
 export default NotFound
