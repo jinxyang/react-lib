7c0996afd17b542bc552067123adfc4480ff3040
jinxyang <jinx@jinxyang.com>
2021-10-29 09:21:53 +0800

    feat: add useKeypress & useSSO

---
+++ src/hooks/useSSO.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { useFetch } from '@jinxyang/react-lib'
+
+const useSSO = (url = '', key = '', onChange = () => {}) => {
+  const value = new URLSearchParams(location.search).get(key)
+  const service = React.useCallback(() => {
+    return {
+      method: 'post',
+      url,
+      data: { [key]: value },
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const [{ loading }, login] = useFetch(service, onChange)
+
+  React.useEffect(() => {
+    value && login()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return loading
+}
+
+export default useSSO
