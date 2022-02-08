9f4443d580d6a989cd56144b4ecbe93a2fcfbd17
jinxyang <jinx@jinxyang.com>
2022-02-08 09:34:11 +0800

    refactor(useLocalStorage): add custom event

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,36 +1,65 @@
 import React from 'react'
+import { nanoid } from 'nanoid'
 
-const useLocalStorage = (key) => {
+import parse from '../utils/parse'
+import stringify from '../utils/stringify'
+
+const useLocalStorage = (keyName, defaultValue, watch = false) => {
+  const currentId = React.useRef(nanoid())
   const [value, setValue] = React.useState(() => {
-    const value = localStorage.getItem(key)
-    try {
-      return value ? JSON.parse(value) : value
-    } catch {
-      return value
-    }
+    return parse(localStorage.getItem(keyName)) ?? defaultValue
   })
 
-  const clean = React.useCallback(() => {
-    localStorage.removeItem(key)
-  }, [key])
+  const dispatch = React.useCallback((value) => {
+    const event = new Event('proStorage')
+    event.id = currentId.current
+    event.key = keyName
+    event.newValue = value
+    window.dispatchEvent(event)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-  const setStorage = React.useCallback(
-    (newValue) => {
-      try {
-        if (newValue === null || newValue === undefined) {
-          clean()
-        } else {
-          const storedValue =
-            typeof newValue === 'object' ? JSON.stringify(newValue) : newValue
-          localStorage.setItem(key, storedValue)
-        }
-      } catch {}
-      setValue(newValue)
+  const clear = React.useCallback(() => {
+    localStorage.removeItem(keyName)
+    dispatch(null)
+    setValue(null)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const set = React.useCallback(
+    (value) => {
+      if (value === null || value === undefined) {
+        clear()
+      } else {
+        localStorage.setItem(keyName, stringify(value))
+        dispatch(value)
+        setValue(value)
+      }
     },
-    [clean, key],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [clear, dispatch],
   )
 
-  return [value, setStorage, clean]
+  const handler = React.useCallback(
+    ({ id, key, newValue }) => {
+      key === keyName && id !== currentId.current && set(newValue)
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [set],
+  )
+
+  React.useEffect(() => {
+    if (!watch) return
+    window.addEventListener('proStorage', handler)
+    window.addEventListener('storage', handler)
+    return () => {
+      window.removeEventListener('proStorage', handler)
+      window.removeEventListener('storage', handler)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return [value, set, clear]
 }
 
 export default useLocalStorage
