b5cc5b537552e997eb0bf19dea2c11868bf241a6
Lijie <18404974997@163.com>
2022-04-18 09:31:10 +0800

    feat(useMenu): add path match pattern

--- src/hooks/useMenu.js
+++ src/hooks/useMenu.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import _ from 'lodash'
 
 import useLocation from './useLocation'
 
@@ -6,9 +7,17 @@ const useMenu = (menus = []) => {
   const location = useLocation()
 
   return React.useMemo(() => {
-    return (
-      menus.find(({ path }) => location.pathname.indexOf(path) === 0) ?? null
+    const matchedMenus = _.filter(
+      menus,
+      ({ path }) => location.pathname.indexOf(path) === 0,
     )
+
+    if (matchedMenus.length === 1) return matchedMenus[0]
+
+    const currentPath =
+      '/' + _.flow(_.split(?, '/'), _.filter(?, Boolean))(location.pathname)[0]
+
+    return _.find(matchedMenus, { path: currentPath }) ?? null
   }, [location.pathname, menus])
 }
 
