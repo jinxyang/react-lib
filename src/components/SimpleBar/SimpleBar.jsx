a8d3431a5d3d2f086561e7b906ea0461e4992699
jinxyang <jinx@jinxyang.com>
2022-04-21 09:09:16 +0800

    refactor(SimpleBar): extract values

--- src/components/SimpleBar/SimpleBar.jsx
+++ src/components/SimpleBar/SimpleBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import _ from 'lodash'
 import { Spin } from 'antd'
 
-import { Flex, View } from '@jinxyang/seal-react'
+import { Flex } from '@jinxyang/seal-react'
 
 import Legend from './Legend'
 
@@ -15,85 +15,100 @@ const defaultColors = [
   '#795548',
   '#795548',
 ]
-const legendMainMap = {
-  left: 'flex-start',
-  right: 'flex-end',
-  center: 'center',
+const layoutDirectionMap = {
+  top: 'column',
+  bottom: 'column-reverse',
+  right: 'row-reverse',
+  left: 'row',
 }
-const directionMap = {
+const legendPositionMap = {
+  left: 'flex-start',
+  center: 'center',
+  right: 'flex-end',
+}
+const barDirectionMap = {
   top: 'column-reverse',
   bottom: 'column',
   right: 'row',
   left: 'row-reverse',
 }
-
 const borderMap = {
   top: 'borderBottom',
   bottom: 'borderTop',
   right: 'borderLeft',
   left: 'borderRight',
 }
-
-const labelOrderMap = {
-  top: 1,
-  bottom: -1,
-  left: 1,
-  right: -1,
-}
-
 const radiusMap = {
   top: ['2px', '2px', 0, 0],
   bottom: [0, 0, '2px', '2px'],
   left: ['2px', 0, 0, '2px'],
   right: [0, '2px', '2px', 0],
 }
-
 const transition = 'all .2s ease'
 
 const Bar = ({
   loading = true,
-  title = null,
   value = [],
   to = 'top',
-  flex = 1,
+  barWidth,
   gap = 1,
+  itemGap = 0.5,
+  boundaryGap = 0.5,
+  minBar = '1px',
   legend = 'topCenter',
   colors: customColors = [],
+  options = {},
 }) => {
+  const { labelFormatter } = options
+
   const [hideNames, setHideNames] = React.useState({})
 
   const names = React.useMemo(() => {
-    return _.map(value, 'name')
+    return _.flow(_.map(?, 'name'), _.filter(?, Boolean))(value)
   }, [value])
 
-  const [legendOrder, legendMain] = React.useMemo(() => {
+  const legendVisible = React.useMemo(() => {
+    return legend && !!names.length
+  }, [legend, names.length])
+
+  const [legendOrder, legendPosition] = React.useMemo(() => {
     const [vertical, horizontal] = _.kebabCase(legend).split('-')
-    return [vertical === 'top' ? -1 : 1, legendMainMap[horizontal]]
+    return [vertical === 'top' ? -1 : 1, legendPositionMap[horizontal]]
   }, [legend])
+
+  const layoutDirection = React.useMemo(() => {
+    return layoutDirectionMap[to]
+  }, [to])
 
   const direction = React.useMemo(() => {
     return to === 'top' || to === 'bottom' ? 'row' : 'column'
   }, [to])
+  const padding = React.useMemo(() => {
+    return direction === 'row' ? [0, boundaryGap] : [boundaryGap, 0]
+  }, [boundaryGap, direction])
 
   const colors = React.useMemo(() => {
     return _.zipWith(defaultColors, customColors, (a, b) => b ?? a)
   }, [customColors])
 
   const maxValue = React.useMemo(() => {
-    return (
-      _.flow(
-        _.map(?, ({ list }) => _.maxBy(list, 'value')?.value),
-        _.max(?),
-      )(value) * 1.1
-    )
+    return _.flow(
+      _.map(?, ({ list }) => _.maxBy(list, 'value')?.value),
+      _.max(?),
+    )(value)
   }, [value])
 
-  const padding = React.useMemo(() => {
-    return direction === 'row' ? [0, 1] : [1, 0]
-  }, [direction])
-
   const data = React.useMemo(() => {
-    return _.flow(_.map(?, 'list'), (value) => _.zip(...value))(value)
+    return _.flow(
+      _.map(?, ({ name, list }) =>
+        _.map(list, ({ value }) => ({ value, name: name ?? '' })),
+      ),
+      (lists) => _.zip(...lists),
+    )(value)
+  }, [value])
+
+  const labels = React.useMemo(() => {
+    return _.map(value[0]?.list ?? [], ({ label, unit }) => ({ label, unit }))
   }, [value])
 
   const handleLegendClick = React.useCallback((name) => {
@@ -103,7 +118,7 @@ const Bar = ({
     }))
   }, [])
 
-  if (!data.length) {
+  if (!_.flatten(data).length) {
     return (
       <Flex
         main="center"
@@ -116,20 +131,9 @@ const Bar = ({
   }
 
   return (
-    <Flex
-      direction="column"
-      styles={{ width: '100%', height: '100%', overflow: 'hidden' }}
-    >
-      {title &&
-        (React.isValidElement(title) ? (
-          title
-        ) : (
-          <Flex.Item order={-2} styles={{ fontSize: 1.1, fontWeight: 'bold' }}>
-            {title}
-          </Flex.Item>
-        ))}
-      {legend && (
-        <Flex main={legendMain} styles={{ order: legendOrder }}>
+    <Flex seal direction="column" styles={{ width: '100%', height: '100%' }}>
+      {legendVisible && (
+        <Flex main={legendPosition} styles={{ order: legendOrder }}>
           <Legend
             names={names}
             hideNames={hideNames}
@@ -138,55 +142,41 @@ const Bar = ({
           />
         </Flex>
       )}
-      <Flex
-        direction={direction === 'row' ? 'column' : 'row'}
-        gap={0.25}
-        styles={{ flex: 1, overflow: 'hidden' }}
-      >
+      <Flex direction={layoutDirection} gap="4px" styles={{ flex: 1 }}>
         <Flex
           direction={direction}
           gap={gap}
           styles={{
-            flex: 1,
+            flex: '0 0 auto',
             padding,
-            border: 0,
-            [borderMap[to]]: 1,
-            borderColor: 'transparent[5]',
           }}
         >
-          {_.map(data, (items, dataIndex) => (
+          {_.map(data, (items, index) => (
             <Flex
-              key={dataIndex}
+              key={index}
               direction={direction}
               main="center"
-              gap="2px"
-              styles={{ flex: 1, transition }}
+              gap={itemGap}
+              styles={{ flex: 1 }}
             >
-              {_.map(items, ({ value: currentValue }, index) => (
+              {_.map(items, ({ value }, index) => (
                 <Flex
                   key={index}
-                  gap={0.5}
-                  direction={directionMap[to]}
-                  cross="center"
+                  direction={barDirectionMap[to]}
+                  main="center"
                   styles={{
-                    flex: hideNames[value[index].name] ? 0 : flex,
-                    width: hideNames[value[index].name] ? 0 : 'auto',
-                    overflow: 'hidden',
+                    flex: hideNames[name]
+                      ? 0
+                      : barWidth != null
+                      ? `0 0 ${barWidth}px`
+                      : 1,
+                    width: hideNames[name] ? 0 : 'auto',
+                    overflow: 'visible',
+                    textAlign: 'center',
                     transition,
                   }}
                 >
-                  <Flex.Item
-                    styles={{
-                      [direction === 'row' ? 'height' : 'width']:
-                        (currentValue / maxValue) * 100 + '%',
-                      [direction === 'row' ? 'width' : 'height']: '100%',
-                      backgroundColor: colors[index],
-                      borderRadius: radiusMap[to],
-                    }}
-                  />
-                  <View styles={{ whiteSpace: 'nowrap', textAlign: 'center' }}>
-                    {currentValue}
-                  </View>
+                  {value}
                 </Flex>
               ))}
             </Flex>
@@ -194,25 +184,75 @@ const Bar = ({
         </Flex>
         <Flex
           direction={direction}
+          gap={gap}
           styles={{
-            flex: '0 0 auto',
+            flex: '1 0 10px',
             padding,
-            order: labelOrderMap[to],
+            border: 0,
+            [borderMap[to]]: 1,
+            borderColor: 'transparent[5]',
           }}
         >
           {_.map(data, (items, index) => (
             <Flex
               key={index}
-              as="span"
+              direction={direction}
               main="center"
+              gap={itemGap}
+              styles={{ flex: 1, transition }}
+            >
+              {_.map(items, ({ name, value }, index) => (
+                <Flex
+                  key={index}
+                  direction={barDirectionMap[to]}
+                  main="flex-start"
+                  styles={{
+                    flex: hideNames[name]
+                      ? 0
+                      : barWidth != null
+                      ? `0 0 ${barWidth}px`
+                      : 1,
+                    width: hideNames[name] ? 0 : 'auto',
+                    transition,
+                  }}
+                >
+                  <Flex.Item
+                    styles={{
+                      [direction === 'row' ? 'height' : 'width']: value
+                        ? (value / maxValue) * 100 + '%'
+                        : minBar,
+                      [direction === 'row' ? 'width' : 'height']: '100%',
+                      backgroundColor: colors[index],
+                      borderRadius: radiusMap[to],
+                      transition,
+                    }}
+                  />
+                </Flex>
+              ))}
+            </Flex>
+          ))}
+        </Flex>
+        <Flex
+          direction={direction}
+          gap={gap}
+          styles={{
+            flex: '0 0 auto',
+            padding,
+          }}
+        >
+          {_.map(labels, (item, index) => (
+            <Flex
+              key={index}
+              direction={barDirectionMap[to]}
+              main="flex-end"
               cross="center"
               styles={{
                 flex: 1,
-                overflow: 'hidden',
                 whiteSpace: 'pre-line',
+                textAlign: 'center',
               }}
             >
-              {items[0].label}
+              {_.isFunction(labelFormatter) ? labelFormatter(item) : item.label}
             </Flex>
           ))}
         </Flex>
